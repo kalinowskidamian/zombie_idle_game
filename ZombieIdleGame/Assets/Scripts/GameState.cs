ba5597d4da789c42f78bd8aaa07cebb115d483da
Damian Kalinowski <91844303+kalinowskidamian@users.noreply.github.com>
2026-03-05 11:18:14 +0100

    Add building-based ectoplasm production and offline gains

--- ZombieIdleGame/Assets/Scripts/GameState.cs
+++ ZombieIdleGame/Assets/Scripts/GameState.cs
@@ -7,6 +7,7 @@ public class GameState
     public long ectoplasm;
     public long bones;
     public long lastSavedUnixSeconds;
+    public double ectoplasmRemainder;
     public List<BuildingInstance> buildingInstances;
 
     public GameState()
@@ -14,6 +15,7 @@ public class GameState
         ectoplasm = 0;
         bones = 0;
         lastSavedUnixSeconds = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        ectoplasmRemainder = 0d;
         buildingInstances = new List<BuildingInstance>();
     }
 }
